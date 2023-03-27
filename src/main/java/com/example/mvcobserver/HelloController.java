a2d108b6eecf29cf246fc37d3a7f7b7c4e23bf31
matand380 <matand94@cs.colman.ac.il>
2023-03-27 20:28:57 +0300

    MVC complex example

--- src/main/java/com/example/mvcobserver/HelloController.java
+++ src/main/java/com/example/mvcobserver/HelloController.java
@@ -9,28 +9,7 @@ import java.util.Observable;
 
 
 public class HelloController extends Observable {
-    @FXML
-    Label resultLabel;
 
 
-    public void generate(){
-        setChanged();
-        notifyObservers();
 
-    }
-
-    public void display(int result){
-        resultLabel.textProperty().set(String.valueOf(result));
-    }
-
-    public void close(ActionEvent actionEvent) {
-        System.exit(0);
-    }
-
-    public void newFile(ActionEvent actionEvent) {
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Open Resource File");
-        fileChooser.showOpenDialog(null);
-
-    }
 }
